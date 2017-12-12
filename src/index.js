94faab39522e69633df8caf8f45f99d632735a43
tmmoon <tmmoon@synapsoft.co.kr>
2017-12-12 13:35:03 +0900

    T02 - 카운터 예제

--- src/index.js
+++ src/index.js
@@ -1,53 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import counterApp from './reducers/index'
 
-/**
- * Action
- */
-const INCREMENT = 'INCREMENT';
+const store = createStore(counterApp);
+const appElement = document.getElementById('root');
 
-function increase(diff) {
-	return {
-		type: INCREMENT,
-		addBy: diff
-	};
-}
-
-/**
- * Reducer
- */
-const initialState = {
-	value: 0
-};
-
-const counterReducer = (state = initialState, action) => {
-	switch(action.type) {
-		case INCREMENT:
-			return Object.assign({}, state, {
-				value: state.value + action.addBy
-			});
-		default:
-			return state;
-	}
-}
-
-/**
- * Store
- */
-const store = createStore(counterReducer);
-
-const render = () => {
-	ReactDOM.render(
-		<App store={store} increase={increase}/>,
-		 document.getElementById('root')
-	);
-};
-
-store.subscribe(render);
-render();
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(
+	<Provider store = {store}>
+		<App/>
+	</Provider>,
+	appElement
+);
